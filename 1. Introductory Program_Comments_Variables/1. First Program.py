af75f31813655d7e7c49f0e80de2e39c796d88cd
Ratan-Debnath <46774994+Ratan-Debnath@users.noreply.github.com>
2019-01-21 11:53:22 -0800

    Add files via upload

---
+++ 1. Introductory Program_Comments_Variables/1. First Program.py
@@ -0,0 +1,20 @@
+print("Welcome to Python programming platform")
+print("Hello, Python!")
+
+#Single line comment
+"""
+Multiple line comments
+
+'''
+this other way to show multiline comments in python3.
+'''
+/*
+This Syntex in other programming language. 
+*/
+
+"""
+#variableName = value [variable declere syntex]
+a = 5
+b = 3.8
+c = a+b
+print(c)
