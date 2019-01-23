cbaa0b45bb52929426d25c6ff5a2b171d636a968
Ratan-Debnath <46774994+Ratan-Debnath@users.noreply.github.com>
2019-01-24 02:03:48 +0600

    update comments and file rename.
    Add comments as variable and print function. Also, include file extension in comments.

--- 1. Introductory Program_Comments_Variables/1. first_program.py
+++ 1. Introductory Program_Comments_Variables/1. first_program.py
@@ -14,7 +14,9 @@ This Syntex in other programming language.
 
 """
 #variableName = value [variable declere syntex]
-a = 5
-b = 3.8
-c = a+b
-print(c)
+a = 5  
+b = 3.8   #where b is a variable which stores value.
+c = a+b   #variable c is store value which adds a and b variable value.
+print(c)  #print is the output function.
+
+#Python files have extension .py.
