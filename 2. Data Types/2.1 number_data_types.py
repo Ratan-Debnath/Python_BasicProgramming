c50aa5e508d6bd09fe340e4cf10407e3388392a8
Ratan-Debnath <46774994+Ratan-Debnath@users.noreply.github.com>
2019-01-24 02:12:52 +0600

    update code with comments.

--- 2. Data Types/2.1 number_data_types.py
+++ 2. Data Types/2.1 number_data_types.py
@@ -1,24 +1,14 @@
 #Python provides 4 numeric data.(int,long,float,complex) to show [Number]
+
 integerNumber = 5 #variableName = value [variable declere syntex]
-largeNumber = 90809080
-floatNumber: float = 10.5
+largeNumber = 90809080 #declere variable without data type
+floatNumber: float = 10.5  #declere variable with data type 
 complexNumber: complex = 2.0 + 2.3j
+
+print(floatNumber) #output this variable value is 10.5
 print(type(integerNumber))
 print(type(largeNumber))
 print(type(floatNumber))
-type(complexNumber)
+print(type(complexNumber) #check type 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+      
